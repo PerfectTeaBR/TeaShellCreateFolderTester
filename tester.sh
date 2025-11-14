a862a8bf518065aacccd88069d5519745a693f94
Aquele Astro Dev <astrochanneloficial@gmail.com>
2025-11-14 18:30:04 -0300

    Update tester.sh

--- tester.sh
+++ tester.sh
@@ -1,2 +1,7 @@
 cd TeaShellCreateFolderTester 
 mkdir src/net/perfect/tea/tester # Lembrando: Isso é só um teste!! Não sei mexer muito com shell :P 
+echo {"As pastas foram criadas!"}
+
+mkdir folders
+echo {"folders adicionados!"}
+git commit -m tudo adicionado!
